324511c4e7fc3c53d460faba9ff18e51c69653f6
tyejae <tyejae@gmail.com>
2017-03-15 10:40:34 -0700

    Adding deletecall command (handles deletecall # and deletecall # for [player name]"

---
+++ commands/clash_caller/delete_call.js
@@ -0,0 +1,68 @@
+const commando = require('discord.js-commando');
+var MESSAGES = require('../../constants/messages.js');
+var REG_EXP = require('../../constants/regular_expressions.js');
+var WAR_INFO = require('../../war_info.js');
+var XMLHttpRequest = require('xhr2');
+
+class DeleteCallCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'deletecall',
+            group: 'clash_caller',
+            memberName: 'deletecall',
+            description: 'Delete call',
+            examples: ['!deletecall #', '!deletecall # for [player name]']
+        });
+    }
+    async run(message, args) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://clashcaller.com/api.php", true);
+        xhr.setRequestHeader("Content-type", 'application/x-www-form-urlencoded');
+        xhr.send("REQUEST=GET_FULL_UPDATE&warcode=" + WAR_INFO.CURRENT_WAR_CODE);
+        xhr.onreadystatechange = function (returnval) {
+            if (xhr.readyState == xhr.DONE && xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                var foundCall = false;
+
+                var options = args.split(' for ');
+                var posy = parseInt(options[0]) - 1;
+                var playername = message.author.username;
+                if (options.length > 1) {
+                    playername = options[1];
+                }
+
+                if (response.calls.length == 0) {
+                    message.channel.sendMessage('No calls for ' + playername + ' on #' + ( posy + 1 ) );
+                } else {
+                    var call = undefined;
+                    for (var cIndex in response.calls) {
+                        call = response.calls[cIndex];
+                        if (call.posy == posy && call.playername == playername) {
+                            foundCall = true;
+                            break;
+                        }
+                    }
+
+                    if (foundCall) {
+                        var xhr2 = new XMLHttpRequest();
+                        var messageSent = false;
+                        xhr2.open("POST", "http://clashcaller.com/api.php", true);
+                        xhr2.setRequestHeader("Content-type", 'application/x-www-form-urlencoded');
+                        xhr2.send("REQUEST=DELETE_CALL&warcode=" + WAR_INFO.CURRENT_WAR_CODE + '&posx=' + call.posx + '&posy=' + call.posy);
+                        xhr2.onreadystatechange = function (returnval) {
+                            if (xhr.readyState == xhr.DONE && xhr.status == 200 && !messageSent) {
+                                message.channel.sendMessage('Deleted call on #' + ( posy + 1 ) + ' for ' + playername);
+                                messageSent = true;
+                            }
+                        }
+                    } else {
+                        message.channel.sendMessage('No calls for ' + playername + ' on #' + ( posy + 1 ) );
+                    }
+                    
+                }
+            }
+        };
+    }
+}
+
+module.exports = DeleteCallCommand;
